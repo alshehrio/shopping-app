652725cb6723d288354acbbc5a99cae6e4e134d8
jaldemad <jaldemad@com.com>
2017-11-06 10:14:30 +0300

    before mosh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
+import { NotFoundComponent } from './component/not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './component/home/home.component';
 import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
 
+export class Paths {
+  static SHOPPING_CART = 'shopping-cart';
+}
+
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'shopping-cart', component: ShoppingCartComponent}
+  { path: '', component: HomeComponent },
+  { path: Paths.SHOPPING_CART, component: ShoppingCartComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(
-      appRoutes
-    )
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
